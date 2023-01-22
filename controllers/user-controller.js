603bf2badcfa23df0aff8362d3ce475abff4cca8
Shibily-kms <kmsshibily@gmail.com>
2023-01-22 20:49:15 +0530

    manifest file add and controller folder create

---
+++ controllers/user-controller.js
@@ -0,0 +1,8 @@
+const userHelpers = require('../helpers/user-helpers');
+const festHelpers = require('../helpers/fest-helpers');
+const groupHelpers = require('../helpers/group-helpers');
+const markHelpers = require('../helpers/mark-helpers')
+const resultHelpers = require('../helpers/result-helpers')
+
+
+module.exports = {}
