853ba27b1c7643c799472f548efbfed6c080cc7f
Muhammad Nishmal P <138182452+Its-me-nishmal@users.noreply.github.com>
2024-01-01 19:52:00 +0530

    Update otp-helpers.js

--- helpers/otp-helpers.js
+++ helpers/otp-helpers.js
@@ -1,39 +1,39 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-const serviceSid = process.env.TWILIO_SERVICE_ID
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
+// const serviceSid = process.env.TWILIO_SERVICE_ID
 
-const dosms = (mobile) => {
-    return new Promise(async (resolve, reject) => {
-        await client.verify.v2.services(serviceSid).verifications.create({
-            to: `+91${mobile}`,
-            channel: 'sms'
-        }).then((response) => {
-            response.valid = true
-            resolve(response)
-        }).catch((err) => {
-            reject(err)
-        })
+// const dosms = (mobile) => {
+//     return new Promise(async (resolve, reject) => {
+//         await client.verify.v2.services(serviceSid).verifications.create({
+//             to: `+91${mobile}`,
+//             channel: 'sms'
+//         }).then((response) => {
+//             response.valid = true
+//             resolve(response)
+//         }).catch((err) => {
+//             reject(err)
+//         })
 
-    })
-}
+//     })
+// }
 
-const otpVerify = (otp, mobile) => {
+// const otpVerify = (otp, mobile) => {
   
-    return new Promise(async (resolve, reject) => {
-        try {
-            await client.verify.services(serviceSid).verificationChecks.create({
-                to: `+91${mobile}`,
-                code: otp
-            }).then((verification) => {
-                resolve(verification.valid)
-            }).catch((err) => {
-                reject(err)
-            })     
-        } catch (error) {
-            reject(err)
-        }
-    })
-}
+//     return new Promise(async (resolve, reject) => {
+//         try {
+//             await client.verify.services(serviceSid).verificationChecks.create({
+//                 to: `+91${mobile}`,
+//                 code: otp
+//             }).then((verification) => {
+//                 resolve(verification.valid)
+//             }).catch((err) => {
+//                 reject(err)
+//             })     
+//         } catch (error) {
+//             reject(err)
+//         }
+//     })
+// }
 
-module.exports = { dosms, otpVerify }  
+// module.exports = { dosms, otpVerify }  
