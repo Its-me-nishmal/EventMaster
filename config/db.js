6e402a967274a4e65e3d76059c3b1942cdd8fd9e
Muhammad Nishmal P <138182452+Its-me-nishmal@users.noreply.github.com>
2024-01-05 11:26:49 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const state = {
 
 module.exports.connect = function (done) {
     const url = 'mongodb+srv://alltrackerx:Nichuvdr%40786@cluster0.zqjk0it.mongodb.net/NSA?retryWrites=true&w=majority'
-    const dbname = 'event'
+    const dbname = 'NSA'
 
 
 
